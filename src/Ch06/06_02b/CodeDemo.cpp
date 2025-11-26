2407ccfc66fdd8de606d41b0aa2a18dfa28ef45c
Kirsch Jan (BEG/ESD1) <Jan.Kirsch@de.bosch.com>
2025-11-26 13:48:28 +0000

    Finish 06 and 07

--- src/Ch06/06_02b/CodeDemo.cpp
+++ src/Ch06/06_02b/CodeDemo.cpp
@@ -4,15 +4,46 @@
 
 #include <iostream>
 
+double multiplyTwoValues(int var1, int var2);
+
+int square(int x){
+    x *= x;
+    return x;
+}
+
+void swap(int *x, int *y){
+    int temp = *x;
+    *x = *y;
+    *y = temp;
+}
+
+void swap(int& x, int& y){
+    int temp = x;
+    x = y;
+    y = temp;
+}
+
 int main(){
     int a = 9, b;
     // TODO: square
+    double multi = 0;
+    multi = multiplyTwoValues(a, 20);
+    b = square(a);
+
+    std::cout << "a * b = " << multi << std::endl;
     std::cout << "a = " << a << ", b = " << b << std::endl;
-    // TODO: swap
+    swap(&a, &b);
     std::cout << "a = " << a << ", b = " << b << std::endl;
-    // TODO: swap
+    
+    swap(a, b);
     std::cout << "a = " << a << ", b = " << b << std::endl;
     
     std::cout << std::endl << std::endl;
     return 0;
 }
+
+double multiplyTwoValues(int var1, int var2){
+    double result = var1 * var2;
+    return result;
+}
+
--- src/Ch06/06_04b/CodeDemo.cpp
+++ src/Ch06/06_04b/CodeDemo.cpp
@@ -5,12 +5,29 @@
 #include <iostream>
 #include <string>
 
-int main(){
-    std::string playerName = "Alex";
-    int score = 75;
 
-    std::cout << playerName << " scored " << score << " points." << std::endl;
+std::string getPlayerName();
+int calculateScore(int baseScore, int multipier);
+void displayScore(const std::string&, int);
+
+int main(){
+    std::string playerName = getPlayerName();
+    int score = calculateScore(75, 2);
+
+    displayScore(playerName, score);
     
     std::cout << std::endl << std::endl;
     return 0;
 }
+
+std::string getPlayerName(){
+    return "Meister";
+}
+
+int calculateScore(int baseScore, int multiplier){
+    return baseScore * multiplier;
+}
+
+void displayScore(const std::string& name, int score){
+    std::cout << name << " scored " << score << " points." << std::endl;
+} 
